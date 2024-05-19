f4f1f802190d78ad09fa7b7cd353c7b83f553765
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-19 23:02:53 +0500

    Add files via upload

---
+++ Q4.js
@@ -0,0 +1,4 @@
+"use strict";
+let quote = "A person who naver made a mistake never tried anything new;";
+let auther = "Albert einstein";
+console.log(`${auther}one said,"${quote}`);
---
+++ Q4.ts
@@ -0,0 +1,3 @@
+let quote:string="A person who naver made a mistake never tried anything new;"
+let auther:string="Albert einstein";
+console.log(`${auther}one said,"${quote}`);
