67ce1967373f995fc3a17612342fc8dbfee03eef
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-19 23:04:16 +0500

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,4 @@
+var quote = "A person who naver made mistake naver tried anything new";
+var famous_person = "Albert einstein";
+var message = "".concat(famous_person, " once said,").concat(quote);
+console.log(message);
