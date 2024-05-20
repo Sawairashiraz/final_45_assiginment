9d9d3c1ae0f80015afda9b4ac61d073134e647bf
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-21 00:12:36 +0500

    Add files via upload

---
+++ Q39.js
@@ -0,0 +1,11 @@
+"use strict";
+function city_country(city, country) {
+    //return `"${city},${country}`"
+    console.log(`"${city},${country}"`);
+}
+city_country("Karachi", "Pakistan");
+let mycities = city_country("Karachi", "Pakistan");
+console.log(mycities);
+console.log(city_country("Dbbai", "UAE"));
+console.log(city_country("London", "United kingdom"));
+console.log(city_country("Riyaz", "Saudi arabia"));
---
+++ Q39.ts
@@ -0,0 +1,11 @@
+function city_country(city:string,country: string){
+    //return `"${city},${country}`"
+    console.log(`"${city},${country}"`)
+
+}
+city_country("Karachi", "Pakistan");
+let mycities = city_country("Karachi", "Pakistan");
+console.log(mycities);
+console.log(city_country("Dbbai","UAE"));
+console.log(city_country("London","United kingdom"));
+console.log(city_country("Riyaz","Saudi arabia"));
