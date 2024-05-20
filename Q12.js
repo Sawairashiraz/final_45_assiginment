8f9f9168edb2b27b8ddc96e1ebb81efa6e698d9d
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-20 19:33:56 +0500

    Add files via upload

---
+++ Q12.js
@@ -0,0 +1,5 @@
+var members = ['sadaf', 'hina', 'waniya', 'ali', 'shaz'];
+var message = "kal meri birthday hai:";
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ Q12.ts
@@ -0,0 +1,5 @@
+let members:string[]=['sadaf','hina','waniya','ali','shaz'];
+let message:string="kal meri birthday hai:";
+for(let i=0; i<members.length; i++){
+    console.log(message+members[i]);
+}
