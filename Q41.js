d02f330c503b417748798e1c37b4fd9825e8aea7
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-21 00:54:28 +0500

    Add files via upload

---
+++ Q41.js
@@ -0,0 +1,8 @@
+"use strict";
+let magician = ["Harrypoter", "Hermione granger", "ron wesly", "samri jadogar"];
+function show_magicians(magicians) {
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+show_magicians(magician);
---
+++ Q41.ts
@@ -0,0 +1,8 @@
+
+let magician = ["Harrypoter", "Hermione granger", "ron wesly", "samri jadogar"];
+function show_magicians(magicians: string[]) {
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+show_magicians(magician);
