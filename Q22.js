7192b28105d0b1236ed8eca70a1246bad240166c
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-20 19:53:20 +0500

    Add files via upload

---
+++ Q22.js
@@ -0,0 +1,3 @@
+var array = ['hina', 'bushra', 'mahwish', 3, 7, 9,'sana','mona,'];
+console.log(array[7]);
+
---
+++ Q22.ts
@@ -0,0 +1,2 @@
+let array:(string | number) []=['hina','bushra','mahwish',3,7,9,'sana','mona',];
+console.log(array[7]);
