a2f1065261b57bbef7ce49d7ac6206f03fed249e
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-21 00:11:11 +0500

    Add files via upload

---
+++ Q38.js
@@ -0,0 +1,15 @@
+"use strict";
+//write a function called describe_city()that accept country and its city. the function
+function describe_city(nameOfCity, country = "south africa") {
+    return `${nameOfCity} is in ${country}`;
+}
+;
+//3 cities
+let city1 = describe_city("captown", "south africa");
+let city2 = describe_city("dehli", "India");
+let city3 = describe_city("Quetta", "Pakistan");
+let city4 = describe_city("UAE", "Dubai");
+console.log(city1);
+console.log(city2);
+console.log(city3);
+console.log(city4);
