ba2af35649763af9746eeef27f63c98caa6f7c97
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-20 19:41:30 +0500

    Add files via upload

---
+++ Q16.ts
@@ -0,0 +1,21 @@
+let guest_list: string []=['shaz','ali','shayan','waniya',];
+
+let not_present :string = 'shayan';
+let new_guest :string = 'shaher';
+guest_list[2]=new_guest;
+for(let i=0; i<guest_list.length; i++){
+    console.log('respected Sir\madam.' + guest_list[i] +',\n\n we inviting you on a dinner tommorow. \n\n thank u\n');
+    }
+    ;
+
+
+    console.log(`Mr.${not_present} will not coming tomorrow dinner.`);
+    console.log('good news! we find big table so we ae inviting 3 more guest.')
+    
+    guest_list.unshift('Maryum');
+guest_list.splice(2,0,'Mahnoor');
+guest_list.push('Sawaira');
+
+for(let i=0; i<guest_list.length; i++){
+    console.log('respected Sir\madam.' + guest_list[i] +',\n\n we inviting you on a dinner tommorow. \n\n thank u\n');
+}
