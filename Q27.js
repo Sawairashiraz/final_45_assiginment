63478e90a51e0f521bbc4de54910a2d89bf69595
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-20 23:31:50 +0500

    Add files via upload

---
+++ Q27.js
@@ -0,0 +1,45 @@
+var alien_color = "green";
+if (alien_color == "green") {
+    console.log("the player just earned 5 points.");
+}
+// if the alien is yellow, print amessage that the player just earned 10 points
+else if (alien_color == "yellow") {
+    console.log("the player just earned 10 points.");
+}
+// if the alien is red, print amessage that the player just earned 15 points
+else if (alien_color == "red") {
+    console.log("the player just earned 15 points.");
+}
+else {
+    console.log("please select right color.");
+}
+// write three version of this program,making sure
+//each message is printed for the appropriate color alien
+//version 2
+alien_color = "red";
+if (alien_color == "green") {
+    console.log("the player just earned 5 points.");
+}
+else if (alien_color == "yellow") {
+    console.log("the player just earned 10 points.");
+}
+else if (alien_color == "red") {
+    console.log("the player just earned 15 points.");
+}
+else {
+    console.log("please select right color.");
+}
+//version 3
+alien_color = "yellow";
+if (alien_color == "green") {
+    console.log("the player just earned 5 points.");
+}
+else if (alien_color == "yellow") {
+    console.log("the player just earned 10 points.");
+}
+else if (alien_color == "red") {
+    console.log("the player just earned 15 points.");
+}
+else {
+    console.log("please select right color.");
+}
