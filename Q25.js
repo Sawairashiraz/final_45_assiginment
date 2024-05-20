0a4bca53275096bf2d097e0b1e024fbce040fcd2
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-20 19:59:28 +0500

    Add files via upload

---
+++ Q25.js
@@ -0,0 +1,11 @@
+let alien_color = "green";
+if (alien_color === "green")
+    [];
+console.log("the player just earne 5 points.");
+//write one version of this program that passes the if test and an other
+//(the version that fails will hve no output.)
+alien_color = "red";
+if (alien_color === "green")
+    [];
+console.log("the player just earne 5 points.");
+export {};
