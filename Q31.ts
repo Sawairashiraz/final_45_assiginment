a3e2afa1d87bb3968e167f8db0fdc9fa1c39f094
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-20 23:40:42 +0500

    Add files via upload

---
+++ Q31.ts
@@ -0,0 +1,15 @@
+//31. no users; add an if test to exercise 30 to
+//make sure the list of users in note empty.
+//.if the list is empty.print the message
+// we need to find csome users.
+
+let users : string [] =['zara','sana', 'waniya','ferwa','admin']
+if (users.length === 0){
+    console.log('we need to find csome users.')
+}
+//.remove all of the usernames from your array.
+//and make sure the correct message is  printed
+else{
+    users =[];
+    console.log('all user have been remove' + users.length)
+}
