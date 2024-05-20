e068c42cdc3c9c9d9566d3e478918bdce36c1b1c
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-20 19:50:17 +0500

    Add files via upload

---
+++ Q20.js
@@ -0,0 +1,6 @@
+var lanuages = ['English', 'Urdu', 'Hindi', 'French', 'Arabic',];
+console.log('list of languages');
+for (var _i = 0, lanuages_1 = lanuages; _i < lanuages_1.length; _i++) {
+    var top_1 = lanuages_1[_i];
+    console.log(top_1);
+}
---
+++ Q20.ts
@@ -0,0 +1,5 @@
+let lanuages :string [] = ['English','Urdu','Hindi','French','Arabic',];
+console.log('list of languages');
+for(let top of lanuages){
+    console.log(top)
+}
