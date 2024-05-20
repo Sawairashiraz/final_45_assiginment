a824853de64a06f04e35dd7a4c7f6de43a094823
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-21 00:14:29 +0500

    Add files via upload

---
+++ Q40.js
@@ -0,0 +1,20 @@
+"use strict";
+function myAlbum(artistName, albumTitle) {
+    return { artistName, albumTitle };
+}
+let album1 = myAlbum("Ali", "janoon");
+let album2 = myAlbum("shaz", "rung");
+let album3 = myAlbum("noor", "azada parinda");
+console.log(album1);
+console.log(album2);
+console.log(album3);
+// number of tracks
+function myAlbum2(artistName, albumTitle, numberOfTracks) {
+    return { artistName, albumTitle, numberOfTracks };
+}
+let album4 = myAlbum2("ali", "dil ha tmhara", 30);
+let album5 = myAlbum2("shaz", "dhoom machaly", 55);
+let album6 = myAlbum2("noor", "khani suno", 45);
+console.log(album4);
+console.log(album5);
+console.log(album6);
