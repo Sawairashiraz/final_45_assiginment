870e253037a14616b9944728904c51c3bdfb91ec
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-20 19:37:10 +0500

    Add files via upload

---
+++ Q13.js
@@ -0,0 +1,4 @@
+var transportation = ['civic', 'bike', 'bus', 'train', 'cycle'];
+for (i = 0; i < transportation.length; i++) {
+    console.log('I wouldlike to own a ' + transportation[i]);
+}
---
+++ Q13.ts
@@ -0,0 +1,5 @@
+let transportation:string[]=['civic','bike','bus','train','cycle'];
+for(i=0;i<transportation.length; i++){
+    console.log('I wouldlike to own a '   +transportation[i]);
+
+}
