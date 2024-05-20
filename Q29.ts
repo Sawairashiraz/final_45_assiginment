c28e9d10dbbbf4fdf6c3815e087295180ac0dad2
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-20 23:36:23 +0500

    Add files via upload

---
+++ Q29.ts
@@ -0,0 +1,14 @@
+let favorite_fruit : string[]= ["apple","mango","orange"]
+if (favorite_fruit.includes("mango")){
+    console.log('you really like mango!')
+}
+if (favorite_fruit.includes("apple")){
+    console.log('you really like apple!')
+}
+if (favorite_fruit.includes("banana")){
+    console.log('you really like banana!')
+}
+if (favorite_fruit.includes("orange")){
+    console.log('you really like orange!')
+}
+
