4793dd5d19a30f8e4186024d90a46bb33c0e0b1e
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-20 20:00:11 +0500

    Add files via upload

---
+++ Q26.js
@@ -0,0 +1,14 @@
+"use strict";
+//chose a color for an alain as you
+//di the Exercises 25 an writean if_else chain.
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log("the playes just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("the playesr just earned 10mpoints.");
+}
+alien_color = "red";
+if (alien_color == "green") {
+    console.log("the player just earned 5 points fror shootig the alien.");
+}
