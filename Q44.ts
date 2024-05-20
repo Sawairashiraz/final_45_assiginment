a3d9fb47fc2a34d776acd71a88585635fc165366
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-21 00:32:26 +0500

    Add files via upload

---
+++ Q44.ts
@@ -0,0 +1,13 @@
+function makeSandwich (item: string[]){
+    console.log('\nMaking your sandwich with:');
+
+    item.forEach(element =>console.log("- " + element));
+
+    console.log('Enjoy your sandwich !\n');
+
+}
+makeSandwich(['ham','Cheese',' lettuce']);
+
+makeSandwich(['Turkey','Bacon',]);
+
+makeSandwich(['Peanut','Butter',' catchup']);
