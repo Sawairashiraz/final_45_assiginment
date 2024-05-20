bbfd8b03977ba582f20f25cc515ad9fe0663fdaa
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-20 19:56:39 +0500

    Add files via upload

---
+++ Q23.ts
@@ -0,0 +1,15 @@
+let car:string ='subaru';
+
+//Test 1:Equality comparison (True)
+console.log("is car == 'subaru'? I predict True.");
+  console.log(car =='subaru'); 
+
+//   Test 2 :strict equality comparison(True)
+console.log("is car === 'subaru'? I predict True.");
+  console.log(car === 'subaru');
+//   Test 3: Inequality (flase)
+console.log("is car != 'subaru'? I predict Flase.");
+  console.log(car !='subaru');
+//    Test 4:strict inequality (false)
+console.log("is car !=='subaru'? I predict False.");
+console .log(car !=='subaru');
